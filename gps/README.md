5a4fdcf32fc87a07c77218fb0d89c134e91f968d
Sanders DeNardi <sedenardi@gmail.com>
2018-02-03 11:03:55 -0500

    add Stamen map

--- gps/README.md
+++ gps/README.md
@@ -5,9 +5,11 @@ All raw and processed GPS tracks, as well as functions for processing them.
 ## Functions
 
 - `routes.js` - Splits a given GPX track into several routes based on entry and exit of a defined "home zone" (hardcoded for now)
+- `convert.js` - Converts, processes, and combines GPX tracks into one GeoJSON `FeatureCollection`
 
 ## TODO
 
+- Determine when route is stopped at delivery
 - Scripted route smoothing based on `gpsbabel` (base simplify count on original number of track points)
 - Interactive "home zone" setting
 - Calculate instantaneous velocity (over several trackpoints), compensate for first/last point outside of "home zone"
--- map/src/js/mapping.js
+++ map/src/js/mapping.js
@@ -9,17 +9,25 @@ const tiles = {
   OpenStreetMap: {
     url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    maxZoom: 19,
-    preferCanvas: true
+    maxZoom: 19
+  },
+  StamenToner: {
+    url: '//stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
+    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY SA</a>.',
+    maxZoom: 19
   }
 };
 
 const init = () => {
-  const map = L.map('map').setView(mapCenter, 14);
-
-  L.tileLayer(tiles.OpenStreetMap.url, {
-    attribution: tiles.OpenStreetMap.attribution,
-    maxZoom: tiles.OpenStreetMap.maxZoom,
+  const map = L.map('map', {
+    center: mapCenter,
+    zoom: 14,
+    // preferCanvas: true
+  });
+  const tile = tiles.StamenToner;
+  L.tileLayer(tile.url, {
+    attribution: tile.attribution,
+    maxZoom: tile.maxZoom
   }).addTo(map);
   return map;
 };
